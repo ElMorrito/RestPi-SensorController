ecfea5446d6062b0c5cb18df4a12170185690703
ElMorrito <numbersliven@hotmail.de>
2021-07-16 17:28:12 +0200

    example data of controller model

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import socket
+
 
 app = Flask("__name__")
 
@@ -8,5 +10,25 @@ def index():
     return "Hello User"
 
 
+# Get Hostname and Ip Address
+hostname = socket.gethostname()
+local_ip_address = socket.gethostbyname(hostname)
+
+
+@app.route('/api/settings', methods=['GET', 'PATCH'])
+def device_settings():
+    if request.method == 'PATCH':
+        return "not implemented"
+    else:
+        return {
+            'id': 1,
+            'host_name': hostname,
+            'ip_address': local_ip_address,
+            'logging': False,
+            'status': 'ok',
+            'location': 'Main Store'
+        }
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
