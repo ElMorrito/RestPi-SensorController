2c4b8d544318c2164f52455c09e8c73996014f93
ElMorrito <numbersliven@hotmail.de>
2021-08-09 13:21:23 +0200

    handle basic login with password hash

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,9 +1,28 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect, url_for, abort
+from flask_login import login_user
+from models import Users
 auth_bp = Blueprint('auth', __name__, template_folder='templates/auth')
 
 
-@auth_bp.route('/login')
-def login_user():
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
 
-    return render_template('login.html')
+    error = None
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = Users.query.filter_by(
+            email=username).first()
+        if not user:
+            error = "User does not exixts"
+            return render_template('login.html', error=error)
+
+        if user.check_password(password):
+            login_user(user=user)
+
+        return redirect(url_for('admin.index'))
+
+    return render_template('login.html', error=error)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from database.database import db
 from datetime import datetime
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class BaseModel(db.Model):
@@ -15,10 +16,18 @@ class BaseModel(db.Model):
 class Users(BaseModel, UserMixin):
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    active = db.Column(db.Boolean())
+    is_active = db.Column(db.Boolean())
+
+    def __init__(self, email, password, is_active=True):
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.is_active = is_active
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
-class Sensor(BaseModel):
+class Sensors(BaseModel):
     name = db.Column(db.String(255), default="", unique=True)
     sensor_id = db.Column(db.String(255), default="",
                           nullable=False, unique=True)
