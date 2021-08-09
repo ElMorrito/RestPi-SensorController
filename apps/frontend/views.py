8761fddc6fab2c67e58645b67527eba138d7a5b3
ElMorrito <numbersliven@hotmail.de>
2021-08-09 11:36:56 +0200

    fix template path

--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template
 
 from apps.utils import hostname, get_local_ip_address
 
-app_blueprint = Blueprint(
-    name="frontend", import_name=__name__, template_folder='/templates/frontend')
+app_blueprint = Blueprint("frontend", __name__,
+                          template_folder='templates/frontend')
 
 
 @app_blueprint.route('/', methods=['GET', ])
