ecb8e9c69b59286a9828185e66d96ae0c7002f82
ElMorrito <numbersliven@hotmail.de>
2021-08-10 09:14:16 +0200

    change function docstring

--- apps/utils.py
+++ apps/utils.py
@@ -10,8 +10,6 @@ def get_local_ip_address():
     '''Get IPv4-Adress of device .
 
     This function can be used for OSX, LINUX and WINDOWS.'''
-    # Ggethostbyname funtion does only return 127.0.1.1 if platfrom is linux,
-    # so check for platfrom first.
     if platform == "linux" or platform == "linux2":
         output = check_output(
             ['hostname', '-s', '-I']).decode('utf-8')[:-1]
@@ -25,7 +23,7 @@ def get_temperature_data(sensor_id: str = None) -> dict:
     dummy_temp = random.uniform(10.1, 25.9)
 
     data = {
-        'value': f'{dummy_temp:.4f}',
-        'unit': '°C'
+        'value': f"{dummy_temp:.4f}",
+        'unit': "°C"
     }
     return data
